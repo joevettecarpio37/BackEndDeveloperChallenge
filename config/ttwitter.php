dd25cab1298dbea1f620826a8020d8f2eb00b784
Joevette Carpio <37733400+joevettecarpio37@users.noreply.github.com>
2018-09-16 15:30:05 +0800

    Update ttwitter.php

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -14,8 +14,8 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '00N4fRqcEmFwHDdfLIjyRBCk8') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', 'PCYtH9SbY4QdSyLX3LvlHgi1BlfAtEF3vbYNeu2RokXrCakOXn') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '1040264806354145281-sQRHJ3z5UuWg8IUdcxmhp8ZfiFIDms') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', 'SmPKP0M1nuxKDIN2QQ5mFuPazkgqvvcxMgOWD6z2knLL3') : '',
+	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
+	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
+	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
+	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
 ];
